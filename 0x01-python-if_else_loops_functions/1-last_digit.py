94c91116ade36395b5b68b12e0ef9f47d30e3136
danrocus1994 <danrodcastillo1994@gmail.com>
2019-12-10 18:29:39 +0000

    das

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,8 +3,9 @@ import random
 number = random.randint(-10000, 10000)
 last = "{}".format(number)
 last = int(last[-1])
-sign = "{si}".format(si="-" if number < 0 and last != 0 else "")
-print("Last digit of {} is {}{} ".format(number, sign, last), end='')
+if number < 0:
+    last = last * (-1)
+print("Last digit of {} is {} ".format(number, last), end='')
 if last > 5:
     print("and is grater than 5")
 elif last < 6 and last != 0:
