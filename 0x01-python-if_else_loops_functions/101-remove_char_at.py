5f7399c569d8f34ca175632a1c58a4400295c1cc
danrocus1994 <danrodcastillo1994@gmail.com>
2019-12-10 22:55:39 -0500

    das

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    pos = 0
     res_arr = []
-    for char in str:
+    for pos, char in enumerate(str):
         if pos is not n:
             res_arr.append(char)
-        pos += 1
     return "".join(res_arr)
