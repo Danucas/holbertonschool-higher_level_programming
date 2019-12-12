9c2369941cc5d82fa3b6e6043da4a280a29d5e40
danrocus1994 <danrodcastillo1994@gmail.com>
2019-12-12 19:44:39 +0000

    d

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 from add_0 import add
-
-
-def main():
-    a = 3
-    b = 4
+if __name__ == '__main__':
+    a = 1
+    b = 2
     res = add(a, b)
     print("{} + {} = {}".format(a, b, res))
-if __name__ == '__main__':
-    main()
