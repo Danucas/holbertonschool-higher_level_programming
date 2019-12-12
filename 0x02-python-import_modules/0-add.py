fed4dec2d04af1982f4151c2db9352cc40c08f20
danrocus1994 <danrodcastillo1994@gmail.com>
2019-12-12 20:08:13 +0000

    da

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-from add_0 import add
+import add_0 as ad
 
 a = 1
 b = 2
 
 if __name__ == '__main__':
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, ad.add(a, b)))
