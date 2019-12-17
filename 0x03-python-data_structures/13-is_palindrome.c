8c808aadcc5bb90fba58d108572c12bf4877fccc
danrocus1994 <danrodcastillo1994@gmail.com>
2019-12-17 18:00:18 -0500

    dsa

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ *is_palindrome - verify if it is a palindrome
+ *@head: the linked list
+ *Return: 1 if it is, 0 if it does not
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *li = *head;
+	int *comp_list;
+	unsigned int length = 0, pos;
+
+	while (li != NULL)
+	{	li = li->next;
+		length++;
+	}
+	li = *head;
+	pos = length;
+	pos--;
+	if (pos > 0)
+	{
+		comp_list = malloc(sizeof(int) * length);
+		while (li != NULL)
+		{
+			comp_list[pos] = li->n;
+			pos--;
+			li = li->next;
+			length++;
+		}
+	}
+	pos = 0;
+	li = *head;
+	while (li != NULL)
+	{
+		if (li->n != comp_list[pos])
+		{
+			return (0);
+		}
+		li = li->next;
+		pos++;
+	}
+	return (1);
+}
