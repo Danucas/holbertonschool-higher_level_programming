651a44b1c70fe94d6487beec885b15465017cdd6
danrocus1994 <danrodcastillo1994@gmail.com>
2019-12-17 18:31:00 -0500

    dsa

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -10,26 +10,34 @@
 int is_palindrome(listint_t **head)
 {
 	listint_t *li = *head;
-	int *comp_list = malloc(sizeof(int) * 1024);
-	unsigned int length = 0;
+	int *comp_list;
+	int length = 0, pos;
 
 	while (li != NULL)
 	{
-		comp_list[length] = li->n;
 		length++;
 		li = li->next;
 	}
 	li = *head;
-	length--;
-	while (li != NULL)
+	comp_list = malloc(sizeof(int) * (length / 2));
+	pos = (length / 2) - 1;
+	while (li != NULL && pos >= 0)
 	{
-		if (li->n != comp_list[length])
+		comp_list[pos] = li->n;
+		li = li->next;
+		pos--;
+	}
+	li = *head;
+	pos = (length / 2) - 1;
+	while (li != NULL && pos >= 0)
+	{
+		if (li->n != comp_list[pos])
 		{
 			free(comp_list);
 			return (0);
 		}
 		li = li->next;
-		length--;
+		pos--;
 	}
 	free(comp_list);
 	return (1);
