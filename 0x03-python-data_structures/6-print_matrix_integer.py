8931a3690ba87b0a0ae1c1f2a28fc4ab696dc366
danrocus1994 <danrodcastillo1994@gmail.com>
2019-12-17 13:28:53 -0500

    dsa

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for line in matrix:
         for i, el in enumerate(line):
-            print("{}".format(el), end='')
+            print("{:d}".format(el), end='')
             if i < (len(line) - 1):
                 print(" ", end='')
         print()
