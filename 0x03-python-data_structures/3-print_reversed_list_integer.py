745040f5cf72afa7834d5f825405c3685c9769e0
danrocus1994 <danrodcastillo1994@gmail.com>
2019-12-17 19:39:50 -0500

    dsa

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if len(my_list) > 0:
+    if my_list and (len(my_list) > 0):
         for i in list(reversed(my_list)):
             print("{:d}".format(i))
