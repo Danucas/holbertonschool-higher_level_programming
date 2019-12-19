d01ae79945ce1a162e64972ec8b593e36b69edb4
danrocus1994 <danrodcastillo1994@gmail.com>
2019-12-19 18:06:43 -0500

    dsa

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    if my_list:
+        new_list = []
+        for el in my_list:
+            if el == search:
+                new_list.append(replace)
+            else:
+                new_list.append(el)
+        return new_list
