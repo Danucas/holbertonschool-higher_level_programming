fc230389b33875c000ce5be6ac8513f70b5c600f
danrocus1994 <danrodcastillo1994@gmail.com>
2019-12-19 18:57:43 -0500

    das

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -6,13 +6,13 @@ def only_diff_elements(set_1, set_2):
         for comp in set_2:
             if el == comp:
                 match = True
-        if match != True:
+        if match is not True:
             new_list.append(el)
     for el in set_2:
         match = False
         for comp in set_1:
             if el == comp:
                 match = True
-        if match != True:
+        if match is not True:
             new_list.append(el)
     return new_list
