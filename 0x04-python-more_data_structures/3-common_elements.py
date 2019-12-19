37efd96d9224efc4f99bb82dc9037aaf4b421d56
danrocus1994 <danrodcastillo1994@gmail.com>
2019-12-19 18:48:47 -0500

    das

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def common_elements(set_1, set_2):
+    common = []
+    for el in set_1:
+        for comp in set_2:
+            if el == comp:
+                common.append(el)
+    return common
