ec456c707d9c5b8ace1e5cd3c7e08eef79400415
danrocus1994 <danrodcastillo1994@gmail.com>
2019-12-19 18:33:31 -0500

    das

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def uniq_add(my_list=[]):
+    sum = 0
+    for i, el in enumerate(my_list):
+        sum = sum + el
+        for j in range(i + 1, len(my_list)):
+            if el == my_list[j]:
+                my_list[j] = 0
+    return sum
