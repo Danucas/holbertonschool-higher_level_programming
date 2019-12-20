1e053672c8df33b047282d47d66feb5b9f82ddda
danrocus1994 <danrodcastillo1994@gmail.com>
2019-12-19 22:14:11 -0500

    dsa

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is not None:
+        highest = 0
+        k = ''
+        for key in a_dictionary:
+            if a_dictionary[key] > highest:
+                highest = a_dictionary[key]
+                k = key
+        return k
