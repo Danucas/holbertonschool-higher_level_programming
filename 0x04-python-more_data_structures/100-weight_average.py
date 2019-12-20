068841d2d63efb850749262d0a550d89c39963af
danrocus1994 <danrodcastillo1994@gmail.com>
2019-12-19 23:10:15 -0500

    dsa

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if len(my_list) > 0:
+        score = 0
+        average = 0
+        for tup in my_list:
+            score = score + (tup[0] * tup[1])
+            average = average + tup[1]
+        return score / average
