82691ab1b5f771593f65556e38240d8bc523aaa0
danrocus1994 <danrodcastillo1994@gmail.com>
2020-01-08 13:36:29 -0500

    dsa

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,9 +5,9 @@ def safe_print_list(my_list=[], x=0):
         printed = 0
         for i in range(x):
                 try:
-                        printed += 1
                         print("{}".format(str(my_list[i])), end="")
-                except:
+                        printed += 1
+                except Exception as e:
                         pass
         if printed > 0:
                 print()
