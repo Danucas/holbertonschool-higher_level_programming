c02b0ce734c8988ca4315ffb5b5f754ec0b3c5f3
danrocus1994 <danrodcastillo1994@gmail.com>
2020-01-08 14:31:38 -0500

    das

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,12 +3,14 @@
 
 def safe_print_list(my_list=[], x=0):
         printed = 0
-        for i in range(x):
-                try:
+        try:
+                for i in range(x):
                         print("{}".format(my_list[i]), end="")
                         printed += 1
-                except Exception as e:
-                        pass
-        if printed > 0:
-                print()
-        return printed
+                if printed > 0:
+                        print()
+                return printed
+        except IndexError:
+                if printed > 0:
+                        print()
+                return printed
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,14 +4,23 @@
 def safe_print_list_integers(my_list=[], x=0):
         printed = 0
         for i in range(x):
+                isint = False
+                outr = True
                 try:
-                        print("{:d}".format(my_list[i]), end="")
-                        printed += 1
-                except Exception as e:
-                        if type(e).__name__ != "IndexError":
-                                pass
-                        else:
-                                raise
-                                exit()
+                        isint = isinstance(my_list[i], int)
+                except:
+                        outr = True
+                        pass
+                #print(isint)
+                if isint is True or outr is True:
+                        try:
+                                print("{:d}".format(my_list[i]), end="")
+                                printed += 1
+                        except Exception as e:
+                                if type(e).__name__ != "IndexError":
+                                                pass
+                                else:
+                                        raise
+                                        exit()
         print()
         return printed
