33a62534a04ef7306334b7ce4883c0699df00787
danrocus1994 <danrodcastillo1994@gmail.com>
2020-01-08 14:55:55 -0500

    dsa

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+        res = None
+        try:
+               res = a / b
+        finally:
+                print("Inside result: {}".format(res))
+                return res
