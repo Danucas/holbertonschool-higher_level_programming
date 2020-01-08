58e4bc4228ea051f889c03ec747e6df22418119b
danrocus1994 <danrodcastillo1994@gmail.com>
2020-01-08 14:49:00 -0500

    dsa

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,26 +1,16 @@
 #!/usr/bin/python3
-
-
 def safe_print_list_integers(my_list=[], x=0):
         printed = 0
-        for i in range(x):
-                isint = False
-                outr = True
-                try:
-                        isint = isinstance(my_list[i], int)
-                except:
-                        outr = True
-                        pass
-                #print(isint)
-                if isint is True or outr is True:
+        try:
+                for i in range(x):
                         try:
                                 print("{:d}".format(my_list[i]), end="")
                                 printed += 1
-                        except Exception as e:
-                                if type(e).__name__ != "IndexError":
-                                                pass
-                                else:
-                                        raise
-                                        exit()
-        print()
-        return printed
+                        except ValueError:
+                                pass
+                        except TypeError:
+                                pass
+                print()
+                return printed
+        except IndexError:
+                raise
