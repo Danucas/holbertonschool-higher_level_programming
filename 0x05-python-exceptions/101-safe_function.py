a36ef8205bcee32515f7f919fbc2a997a9dad1f4
danrocus1994 <danrodcastillo1994@gmail.com>
2020-01-08 19:54:43 -0500

    dsa

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    res = None
+    try:
+        res = fct(*args)
+        return res
+    except Exception as e:
+        print("Exception: {}".format(e.args[0]))
+        return res
