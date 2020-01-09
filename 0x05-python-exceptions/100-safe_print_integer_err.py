bf66d7cf483420cf029f880e2fca0cbc34e1d8f9
danrocus1994 <danrodcastillo1994@gmail.com>
2020-01-08 19:00:32 -0500

    dsa

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except TypeError as e:
+        print("Exception: ", e.args[0], file=sys.stderr)
+        return False
+    except ValueError as e:
+        print("Exception: ", e.args[0], file=sys.stderr)
+        return False
