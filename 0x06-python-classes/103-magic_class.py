ec66378acb220d91a5e9e15b7780b17f1614e43f
danrocus1994 <danrodcastillo1994@gmail.com>
2020-01-12 12:41:42 -0500

    dsa

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -5,6 +5,7 @@ class MagicClass:
 
     """No lo se"""
     def __init__(self, radius):
+        """init function"""
         self.__radius=0
 
         if type (radius) is not int and type (radius) is not float:
@@ -14,7 +15,9 @@ class MagicClass:
             self.__radius=radius
 
     def area ():
+        """area function"""
         return self.__radius**2*math.pi
     
     def circumference():
+        """circumference function"""
         return 2*math.pi*self.__radius
