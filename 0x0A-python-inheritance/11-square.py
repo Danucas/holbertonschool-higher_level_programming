26df4017376c1e493b3946b8a87c688a35972bc3
danrocus1994 <danrodcastillo1994@gmail.com>
2020-01-20 19:01:53 -0500

    dsa

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""square module"""
+
+
+Rectangle = __import__("9-rectangle").Rectangle
+class Square(Rectangle):
+    """square class"""
+    def __init__(self, size):
+        """init function"""
+        super().integer_validator("size", size)
+        self._Rectangle__width = size
+        self._Rectangle__height = size
+        self.__size = size
+
+    def __str__(self):
+        """str"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
---
+++ 0x0A-python-inheritance/11-square.py~
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""square module"""
+
+
+Rectangle = __import__("9-rectangle").Rectangle
+class Square(Rectangle):
+    """square class"""
+    def __init__(self, size):
+        """init function"""
+        super().integer_validator("size", size)
+        self._Rectangle__width = size
+        self._Rectangle__height = size
