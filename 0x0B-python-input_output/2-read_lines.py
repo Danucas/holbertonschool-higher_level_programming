482a11350211edd121a1b4c7b7f6e45afac7e4ae
danrocus1994 <danrodcastillo1994@gmail.com>
2020-01-21 20:50:10 -0500

    dsa

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -6,6 +6,6 @@ def read_lines(filename="", nb_lines=0):
     with open(filename, "r", encoding="utf-8") as f:
         lines = f.read().split("\n")
         if nb_lines <= 0:
-            nb_lines = len(lines)
+            nb_lines = len(lines) - 1
         for i in range(nb_lines):
             print(lines[i])
