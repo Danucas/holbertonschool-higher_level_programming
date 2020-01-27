b0f8135e43fb0a81e61d9eabd8295b1f7255b5bc
danrocus1994 <danrodcastillo1994@gmail.com>
2020-01-27 02:33:35 -0500

    dsa

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -9,6 +9,7 @@ class Base:
     """Base class"""
     __nb_objects = 0
     def __init__(self, id=None):
+        """init function"""
         if id is not None:
             self.id = id
         else:
@@ -17,5 +18,16 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        res = [json.dumps(x) for x in list_dictionaries]
+        """to json string"""
+        res = json.dumps(list_dictionaries)
         return res
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """parse to json an write the value on a file"""
+        if list_objs is not None:
+            if all(issubclass(cls, Base) for x in list_objs):
+                dics = [di.to_dictionary() for di in list_objs]
+                string = cls.to_json_string(dics)
+                with open(cls.__name__ + ".json", "w") as file:
+                    file.write(string)
