900b26902976b4df7214dfd32c64b9f3e818e68c
danrocus1994 <danrodcastillo1994@gmail.com>
2020-01-27 21:16:50 -0500

    dsa

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -20,14 +20,13 @@ class Square(Rectangle):
     @size.setter
     def size(self, value):
         """size getter"""
-        if type(value) == int:
-            if value < 0:
-                raise ValueError("width must be > 0")
-            self.width = value
-            self.height = value
-            self.__size = value
-        else:
+        if type(value) != int:
             raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        self.width = value
+        self.height = value
+        self.__size = value
 
     def __str__(self):
         """return string representation of rectangle"""
