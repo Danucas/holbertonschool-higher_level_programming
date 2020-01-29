a95b9cf0f61d2af42d9aaf03c0d3fa89482dfacc
danrocus1994 <danrodcastillo1994@gmail.com>
2020-01-28 23:06:42 -0500

    dsa

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -5,6 +5,7 @@
 import unittest
 import sys
 import os
+import json
 from models.square import Square
 
 
@@ -76,3 +77,117 @@ class TestSquare(unittest.TestCase):
     def test_str_exists(self):
         """str exists?"""
         self.assertTrue("__str__" in dir(self.sq))
+
+    def test_to_dictionary(self):
+        """to dic"""
+        dic = self.sq.to_dictionary()
+        self.assertTrue(type(dic) == dict)
+
+    def test_update(self):
+        """update test"""
+        self.assertTrue("update" in dir(self.sq))
+
+    def test_update_1(self):
+        """update test"""
+        self.sq.update(89)
+        self.assertEqual(self.sq.id, 89)
+
+    def test_update_2(self):
+        """update test"""
+        self.sq.update(89, 1)
+        self.assertEqual(self.sq.size, 1)
+        self.assertEqual(self.sq.id, 89)
+
+    def test_update_3(self):
+        """update test"""
+        self.sq.update(89, 1, 2)
+        self.assertEqual(self.sq.x, 2)
+        self.assertEqual(self.sq.size, 1)
+        self.assertEqual(self.sq.id, 89)
+
+    def test_update_4(self):
+        """update test"""
+        self.sq.update(89, 1, 2, 3)
+        self.assertEqual(self.sq.y, 3)
+        self.assertEqual(self.sq.x, 2)
+        self.assertEqual(self.sq.size, 1)
+        self.assertEqual(self.sq.id, 89)
+
+    def test_update_kwargs(self):
+        """update test"""
+        self.sq.update(**{'id': 89})
+        self.assertEqual(self.sq.id, 89)
+
+    def test_update_kwargs_1(self):
+        """update test"""
+        self.sq.update(**{'id': 89, 'size': 1})
+        self.assertEqual(self.sq.size, 1)
+        self.assertEqual(self.sq.id, 89)
+
+    def test_update_kwargs_2(self):
+        """update test"""
+        self.sq.update(**{'id': 89, 'size': 1, 'x': 2})
+        self.assertEqual(self.sq.x, 2)
+        self.assertEqual(self.sq.size, 1)
+        self.assertEqual(self.sq.id, 89)
+
+    def test_update_kwargs_3(self):
+        """update test"""
+        self.sq.update(**{'id': 89, 'size': 1, 'x': 2, 'y': 3})
+        self.assertEqual(self.sq.y, 3)
+        self.assertEqual(self.sq.x, 2)
+        self.assertEqual(self.sq.size, 1)
+        self.assertEqual(self.sq.id, 89)
+
+    def test_create_1(self):
+        """create testing"""
+        self.sq = Square.create(**{'id': 89})
+        self.assertEqual(type(self.sq), Square)
+        self.assertEqual(self.sq.id, 89)
+
+    def test_create_2(self):
+        """create testing"""
+        self.sq = Square.create(**{'id': 89, 'size': 1})
+        self.assertEqual(type(self.sq), Square)
+        self.assertEqual(self.sq.size, 1)
+        self.assertEqual(self.sq.id, 89)
+
+    def test_create_3(self):
+        """create testing"""
+        self.sq = Square.create(**{'id': 89, 'size': 1, 'x': 2})
+        self.assertEqual(type(self.sq), Square)
+        self.assertEqual(self.sq.x, 2)
+        self.assertEqual(self.sq.size, 1)
+        self.assertEqual(self.sq.id, 89)
+
+    def test_create_4(self):
+        """create testing"""
+        self.sq = Square.create(**{'id': 89, 'size': 1, 'x': 2, 'y': 3})
+        self.assertEqual(type(self.sq), Square)
+        self.assertEqual(self.sq.y, 3)
+        self.assertEqual(self.sq.x, 2)
+        self.assertEqual(self.sq.size, 1)
+        self.assertEqual(self.sq.id, 89)
+
+    def test_save_to_file_1(self):
+        """save to file testing"""
+        Square.save_to_file(None)
+        with open("Square.json", "r") as f:
+            txt = f.read()
+            self.assertEqual(txt, "[]")
+
+    def test_save_to_file_2(self):
+        """save to file testing"""
+        Square.save_to_file([])
+        with open("Square.json", "r") as f:
+            txt = f.read()
+            self.assertEqual(txt, "[]")
+
+    def test_save_to_file_3(self):
+        """save to file testing"""
+        Square.save_to_file([Square(1)])
+        with open("Square.json", "r") as f:
+            txt = f.read()
+            obj = json.loads(txt)
+            self.assertEqual(type(obj), list)
+            self.assertEqual(type(obj[0]), dict)
