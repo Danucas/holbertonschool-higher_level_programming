1a3633afd96485c6cc43c1e58221b2989946bd46
danrocus1994 <danrodcastillo1994@gmail.com>
2020-03-04 04:29:39 -0500

    dsa

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,3 @@
+-- average temperatures
+SELECT city, AVG(value) AS avg_temp FROM temperatures
+GROUP BY city ORDER BY avg_temp DESC;
---
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -0,0 +1,4 @@
+-- select max state
+SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state
+ORDER BY state ASC
+LIMIT 3;
