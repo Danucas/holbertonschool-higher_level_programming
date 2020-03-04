810510368a9359e0f8571f351d31d8c35e075e54
danrocus1994 <danrodcastillo1994@gmail.com>
2020-03-04 04:22:10 -0500

    dsa

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,3 +1,3 @@
---state creation on sql db
+-- state creation on sql db
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL);
