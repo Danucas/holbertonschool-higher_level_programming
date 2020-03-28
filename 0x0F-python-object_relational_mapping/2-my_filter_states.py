4d458d1edff3089e45df2e90317afbf1296c5609
danrocus1994 <danrodcastillo1994@gmail.com>
2020-03-28 19:13:22 +0000

    query that accepts an argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Select the mysql table states by name starting with N
+"""
+
+import MySQLdb
+import sys
+
+
+def main():
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    argument = sys.argv[4]
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         db=database,
+                         user=username,
+                         passwd=password)
+    c = db.cursor()
+    c.execute("""SELECT * FROM states WHERE CONVERT(states.name
+    , BINARY) = CONVERT('{}', BINARY) ORDER BY id ASC""".format(argument))
+    for tup in c.fetchall():
+        print(tup)
+
+if __name__ == '__main__':
+    main()
