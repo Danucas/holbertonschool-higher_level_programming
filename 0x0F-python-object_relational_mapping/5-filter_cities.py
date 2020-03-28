ad77553e144751e709901e46be92eac5b278db13
danrocus1994 <danrodcastillo1994@gmail.com>
2020-03-28 21:36:08 +0000

    cities by state argument

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Select the mysql table states by name starting with N
+"""
+
+import MySQLdb
+import sys
+
+
+def main():
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    argument = sys.argv[4]
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         db=database,
+                         user=username,
+                         passwd=password)
+    c = db.cursor()
+    c.execute("""SELECT
+                      cities.name
+                 FROM
+                      cities, states
+                 WHERE
+                      cities.state_id=(
+                             SELECT
+                                  states.id
+                             FROM
+                                  states
+                             WHERE
+                                  states.name=%(state)s)
+                 ORDER BY
+                      cities.id ASC""", {
+                          'state': argument
+                      })
+    res = {city[0] for city in c.fetchall()}
+    print(', '.join(res))
+
+if __name__ == '__main__':
+    main()
