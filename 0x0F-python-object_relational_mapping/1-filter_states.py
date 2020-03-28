521b06c261d35daaae337b032a063fe8b964eaf3
danrocus1994 <danrodcastillo1994@gmail.com>
2020-03-28 18:23:21 +0000

    filter by first character

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -17,7 +17,7 @@ def main():
                          user=username,
                          passwd=password)
     c = db.cursor()
-    c.execute("SELECT * FROM states ORDER BY id ASC where name like 'N%'")
+    c.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     for tup in c.fetchall():
         print(tup)
 
