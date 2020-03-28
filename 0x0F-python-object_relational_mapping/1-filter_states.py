4735d0d7590de669263a9bb59cf69fba3f133e41
danrocus1994 <danrodcastillo1994@gmail.com>
2020-03-28 18:14:40 +0000

    filter by first character

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Select the mysql table states by name starting with N
+"""
+
+import MySQLdb
+import sys
+
+
+def main():
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         db=database,
+                         user=username,
+                         passwd=password)
+    c = db.cursor()
+    c.execute("SELECT * FROM states where name like 'N%'")
+    for tup in c.fetchall():
+        print(tup)
+
+if __name__ == '__main__':
+    main()
