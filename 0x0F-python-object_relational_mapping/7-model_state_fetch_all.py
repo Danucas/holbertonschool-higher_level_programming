445d803e236c21d0476e7643be9e62609cc97988
danrocus1994 <danrodcastillo1994@gmail.com>
2020-03-29 04:09:49 +0000

    Alchemy SQL states Query

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Lists all states in ascending order
+"""
+
+
+from sqlalchemy import *
+import warnings
+from sqlalchemy import exc as sa_exc
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+
+def main():
+    engine = create_engine('mysql+mysqldb:' +
+                           '//{}:{}@localhost/{}'.format(sys.argv[1],
+                                                         sys.argv[2],
+                                                         sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    with warnings.catch_warnings(record=True) as w:
+        warnings.simplefilter("always")
+        states = session.query(State).all()
+    for model in states:
+        print(model)
+
+if __name__ == '__main__':
+    main()
