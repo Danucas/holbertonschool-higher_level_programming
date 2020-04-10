fc17fc9883cb096a37c726455b9d452acb9da186
danrocus1994 <danrodcastillo1994@gmail.com>
2020-04-09 22:27:11 -0500

    sda

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,16 +3,26 @@
 Finds a peak in a list
 """
 
-def find_peak(signal):
+
+def find_peak(list_of_integers):
     """
     Finds a peak in a list
     """
-    peak = None
-    for i, num in enumerate(signal):
-        try:
-            if i > 0 and i < len(signal) - 1:
-                if num >= signal[i-1] and num >= signal[i+1]:
-                    peak = num
-        except:
-            pass
-    return peak
+    if not list_of_integers:
+        return None
+    return rec(list_of_integers, 0, len(list_of_integers) - 1,
+               len(list_of_integers))
+
+
+def rec(l, lw, hg, n):
+    """
+    recursive algo
+    """
+    m = lw + (hg - lw) // 2
+    if (m == 0 or l[m - 1] <= l[m]) and\
+       (m == n - 1 or l[m + 1] <= l[m]):
+        return l[m]
+    elif (m > 0 and l[m - 1] > l[m]):
+        return rec(l, lw, m - 1, n)
+    else:
+        return rec(l, m + 1, hg, n)
