ad896644d360a22f696ff173378d3e8dc0683830
danrocus1994 <danrodcastillo1994@gmail.com>
2020-04-15 05:51:09 +0000

    json_api and github id

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Python script to fetch an https request
+"""
+
+
+import requests
+import sys
+
+
+def main():
+    url = "https://api.github.com/user"
+    data = (sys.argv[1], sys.argv[2])
+    response = requests.get(url, auth=data)
+    js = response.json()
+    if 'id' in js:
+        print('{}'.format(js['id']))
+
+if __name__ == '__main__':
+   main()
+    
