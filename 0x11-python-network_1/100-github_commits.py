91b368481f99164b0a57368c8f96ab5d29be8c7a
danrocus1994 <danrodcastillo1994@gmail.com>
2020-04-15 06:20:08 +0000

    commits from rails

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Python script to fetch an https request
+"""
+
+
+import requests
+import sys
+
+
+def main():
+    url = "https://api.github.com/repos/{}/{}/commits"
+    url = url.format(sys.argv[1], sys.argv[2])
+    response = requests.get(url)
+    commits = response.json()
+    for com in commits[:10]:
+        print(com['sha'], com['commit']['author']['name'])
+
+if __name__ == '__main__':
+   main()
+    
