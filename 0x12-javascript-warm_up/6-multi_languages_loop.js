4aab09a9f0f875c7186438f4b50278792d80bb73
danrocus1994 <danrodcastillo1994@gmail.com>
2020-05-05 22:24:09 +0000

    multilanguages forEach loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const cStr = 'C is fun';
+const pyStr = 'Python is cool';
+const myVar = 'Javascript is amazing';
+const arr = [cStr, pyStr, myVar];
+arr.forEach(language => {
+  console.log(language);
+});
