c1fa53f34c03cce44040c236d38ef043d8f7a2b1
danrocus1994 <danrodcastillo1994@gmail.com>
2020-05-06 03:49:07 +0000

    the second biggest

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+const a = Number(process.argv[2]);
+const b = Number(process.argv[3]);
+
+function findSecondBiggest () {
+  let last = a;
+  let novo = a;
+  let actual;
+  for (let i = 2; i < process.argv.length; i++) {
+    actual = Number(process.argv[i]);
+    if (actual > novo) {
+      last = novo;
+      novo = actual;
+    } else if (actual > last) {
+      last = actual;
+    }
+  }
+  return (last);
+}
+
+if (isNaN(a)) {
+  console.log('0');
+} else if (isNaN(b)) {
+  console.log('0');
+} else {
+  console.log(findSecondBiggest());
+}
