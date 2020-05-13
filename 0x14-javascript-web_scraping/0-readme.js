96ad472168d86f1ff369c74e1ca7c20c796af914
danrocus1994 <danrodcastillo1994@gmail.com>
2020-05-12 23:57:09 -0500

    writing to stderr

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-
+const filename = process.argv[2];
 try {
-  const filename = process.argv[2];
   const content = fs.readFileSync(filename, 'utf-8');
   console.log(content);
 } catch (err) {
-  delete err.stack;
-  console.log(err);
+  console.error(err);
 }
