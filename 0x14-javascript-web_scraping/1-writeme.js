c88baa11d6addcc5bf984e653c9be7942a45f309
danrocus1994 <danrodcastillo1994@gmail.com>
2020-05-13 00:17:35 -0500

    write

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const filename = process.argv[2];
+const data = process.argv[3];
+fs.writeFile(filename, data, 'utf-8', function (err, res) {
+  if (err) {
+    return console.log(err);
+  }
+});
