25fe83a07ccdbe4583f1075861b41bc80dfc16b1
danrocus1994 <danrodcastillo1994@gmail.com>
2020-05-12 23:37:18 -0500

    read a file with node

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+try {
+  const filename = process.argv[2];
+  const content = fs.readFileSync(filename, 'utf-8');
+  console.log(content);
+} catch (err) {
+  console.log(err);
+}
